437d263e94401e51fb23d04bd1024dd130a7c772
Gabriel Montenegro <montenegrogabriel351@gmail.com>
2022-08-12 14:12:53 -0300

    Category initial

---
+++ src/test/java/com/montenegro/productCategoryManage/ProductCategoryManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montenegro.productCategoryManage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCategoryManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
